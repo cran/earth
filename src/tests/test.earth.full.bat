1efc41289db1a9c487787faff65187d61d6c5527
Stephen Milborrow <milbo@sonic.net>
2007-04-23 16:02:07 +0000

    version 0.1-3

--- src/tests/test.earth.full.bat
+++ src/tests/test.earth.full.bat
@@ -1,7 +1,7 @@
 @rem test.earth.full.bat: this does a regression test of earth
 @rem Stephen Milborrow Apr 2007 Petaluma
 
-"C:\a1\r\work\bin\Rcmd.exe" BATCH --quiet --vanilla test.earth.full.R
+"c:\Program Files\r\R-2.4.1\bin\Rcmd.exe" BATCH --quiet --vanilla test.earth.full.R
 @if %errorlevel% equ 0 goto good1:
 @echo error: R returned errorlevel %errorlevel%, see test.earth.full.Rout
 @exit /B %errorlevel%
